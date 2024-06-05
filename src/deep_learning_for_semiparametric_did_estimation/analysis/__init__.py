2131917ceaa4537a384ed0cdac755a0de4e7eb93
NormProgr <s6nometz@uni-bonn.de>
2024-06-05 12:08:34 +0200

    implemented classif drouble robust estimation

--- src/deep_learning_for_semiparametric_did_estimation/analysis/__init__.py
+++ src/deep_learning_for_semiparametric_did_estimation/analysis/__init__.py
@@ -1,1 +1,6 @@
 """Code for the core analyses."""
+from deep_learning_for_semiparametric_did_estimation.analysis.dr_did import (
+    estimate_dr_ATTE,
+)
+
+__all__ = [estimate_dr_ATTE]
--- src/deep_learning_for_semiparametric_did_estimation/analysis/task_analysis.py
+++ src/deep_learning_for_semiparametric_did_estimation/analysis/task_analysis.py
@@ -1,7 +1,42 @@
 """Tasks running the core analyses."""
 
 from pathlib import Path
+from typing import Annotated
 
-import pytask
+import pandas as pd
+from pytask import Product
 
-from deep_learning_for_semiparametric_did_estimation.config import BLD, GROUPS, SRC
+from deep_learning_for_semiparametric_did_estimation.analysis import estimate_dr_ATTE
+from deep_learning_for_semiparametric_did_estimation.config import BLD
+
+
+def make_task_estimate_regression(dgp):
+    def task_estimate_regression(
+        did_table_year_output: Annotated[Path, Product] = BLD
+        / "deep_learning_for_semiparametric_did_estimation"
+        / "tables"
+        / f"dr_classic_results_{dgp}.tex",
+    ) -> None:
+        model_results = estimate_dr_ATTE(
+            n_obs=1000,
+            n_rep=100,
+            ATTE=0.0,
+            dgp_type=dgp,
+        )  # change n_rep to 100
+        df = pd.DataFrame(list(model_results.items()), columns=["Measure", "Value"])
+        with open(did_table_year_output, "w") as fh:
+            fh.write(df.to_latex())
+
+    return task_estimate_regression
+
+
+dgp_type = [1, 2, 3, 4]
+
+tasks = {}
+for dgp in dgp_type:
+    tasks[f"task_estimate_regression_{dgp}"] = make_task_estimate_regression(dgp)
+
+# Call the tasks to execute them
+for task_name, task in tasks.items():
+    print(f"Running {task_name}...")
+    task()
